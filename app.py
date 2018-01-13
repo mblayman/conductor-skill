d13454947d99f4970f052d44b4e6a3a802c03a5e
Matt Layman <matthewlayman@gmail.com>
2018-01-13 00:19:55 -0500

    Make sure only my skill will be accepted by the endpoint.

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+# Add protection from other skills.
+skill_id = 'amzn1.ask.skill.d4d0d1bc-2c5f-4fff-96da-dc2cc927a8ef'
+app.config['ASK_APPLICATION_ID'] = skill_id
+
 
 @ask.intent('GetSchoolDeadlinesIntent')
 def get_school_deadlines(college):
