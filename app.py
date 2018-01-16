35ddbe33221004e3dbcefd87a69ee83af11117c3
Matt Layman <matthewlayman@gmail.com>
2018-01-16 18:07:52 -0500

    Rollbar integration (#1)
    * Install Rollbar.
    
    Blinker is also required because its presence activates signal support
    in Flask.
    
    * Add the in-progress Rollbar support.
    
    * Get Rollbar working.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+import os
+
+from flask import Flask, got_request_exception, request
 from flask_ask import Ask, question, statement
+import rollbar
+import rollbar.contrib.flask
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -13,6 +17,25 @@ DEADLINES = [
     'For the University of Virginia, the regular decision deadline is January 1, 2018.',
     'For Yale University, the regular decision deadline is January 2, 2018.',
 ]
+
+
+def report_exception(app, exception):
+    rollbar.report_exc_info(request=request)
+    # Wait or else Rollbar won't finish before the Lambda goes away.
+    rollbar.wait()
+
+
+@app.before_first_request
+def init_rollbar():
+    """Initialize Rollbar."""
+    rollbar.init(
+        os.environ['ROLLBAR_ACCESS_TOKEN'],
+        os.environ['STAGE'],
+        # Flask already sets up logging.
+        allow_logging_basic_config=False)
+
+    # Send exceptions from `app` to Rollbar, using Flask's signal system.
+    got_request_exception.connect(report_exception, app)
 
 
 @ask.launch
@@ -59,3 +82,7 @@ def help():
 @ask.intent('AMAZON.StopIntent')
 def stop():
     return statement('Goodbye')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
